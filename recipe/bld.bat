7168b1263b3dccda9bd0147b96857f56859e07a2
Vishal Bharadwaj <vsv12mar@gmail.com>
2025-07-01 19:36:58 +0000

    2.5.0rc0 conda-build fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,3 +3,32 @@ if errorlevel 1 exit 1
 
 "%PYTHON%" -m pip install -Ue .
 if errorlevel 1 exit 1
+
+@REM everything below this is an addition check
+@REM @echo on
+@REM setlocal ENABLEEXTENSIONS
+@REM set FC=%FORTRAN%
+
+@REM REM Compile Fortran
+@REM %FC% /O2 /c reg_tet.f
+@REM if errorlevel 1 exit 1
+
+@REM REM Get Python include path
+@REM for /f %%i in ('"%PYTHON%" -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())"') do set PYTHON_INC=%%i
+@REM for /f %%i in ('"%PYTHON%" -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))"') do set PYTHON_LIBDIR=%%i
+@REM for /f %%i in ('"%PYTHON%" -c "import sysconfig; print(sysconfig.get_config_var('LDLIBRARY'))"') do set PYTHON_LIB=%%i
+
+@REM REM Compile C++ code
+@REM cl /c /O2 /EHsc /MD /I %PYTHON_INC% hpbmodule.cpp
+@REM if errorlevel 1 exit 1
+
+@REM REM Link to make DLL
+@REM link /DLL /OUT:hpb.pyd hpbmodule.obj reg_tet.obj /LIBPATH:%PYTHON_LIBDIR% %PYTHON_LIB% gfortran.lib
+@REM if errorlevel 1 exit 1
+
+@REM REM Move DLL to correct location
+@REM copy hpb.pyd ..
+
+@REM REM Final install
+@REM "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
+@REM if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,76 @@
+# #!/bin/bash
+# set -ex
+
+# cd prody/proteins/hpbmodule/
+
+# # Compile Fortran code
+# $FC -O3 -fPIC -c reg_tet.f
+
+# # Get Python include and lib details
+# PYTHON_INCLUDE=$($PYTHON -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+# PYTHON_LIBDIR=$($PYTHON -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+# PYTHON_LIB=$($PYTHON -c "import sysconfig; print(sysconfig.get_config_var('LDLIBRARY'))")
+# PYTHON_LIB_BASENAME=${PYTHON_LIB#lib}
+# PYTHON_LIB_NAME=$(basename "${PYTHON_LIB}" | sed -E 's/^lib(.*)\.(so|dylib|a)$/\1/')
+#  # Works for .so or .dylib
+
+# # Compile C++ code
+# g++ -O3 -g -fPIC -c hpbmodule.cpp -o hpbmodule.o -I${PYTHON_INCLUDE}
+
+# # Export library path for linker
+# export LIBRARY_PATH=$PREFIX/lib:$PYTHON_LIBDIR:$LIBRARY_PATH
+
+# # Link shared library
+# if [[ "$(uname)" == "Darwin" ]]; then
+#   g++ -dynamiclib -o hpb.so hpbmodule.o reg_tet.o -L${PYTHON_LIBDIR} -l${PYTHON_LIB_NAME} -lgfortran
+# else
+#   g++ -shared -Wl,-soname,hpb.so -o hpb.so hpbmodule.o reg_tet.o -L${PYTHON_LIBDIR} -l${PYTHON_LIB_NAME} -lgfortran
+# fi
+
+# # Move built .so file to destination
+# cp hpb.so ../
+
+# cd -
+
+# # Final installation
+# $PYTHON setup.py build_ext --force
+# $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+#!/bin/bash
+set -ex
+export CFLAGS="${CFLAGS} -D__NO_FLOAT16"
+export CPPFLAGS="${CPPFLAGS} -D__NO_FLOAT16"
+
 cd prody/proteins/hpbmodule/
-gfortran -O3 -fPIC -c reg_tet.f
-variable=$(python -c 'from distutils.sysconfig import get_python_inc; print(get_python_inc())')
-g++ -O3 -g -fPIC -c hpbmodule.cpp -o hpbmodule.o -I/$variable
-g++ -shared -Wl,-soname,hpb.so -o hpb.so hpbmodule.o reg_tet.o -lgfortran
+
+# Compile Fortran code
+$FC -O3 -fPIC -c reg_tet.f
+
+# Get Python include and lib details
+PYTHON_INCLUDE=$($PYTHON -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+PYTHON_LIBDIR=$($PYTHON -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+PYTHON_LIB=$($PYTHON -c "import sysconfig; print(sysconfig.get_config_var('LDLIBRARY'))")
+PYTHON_LIB_BASENAME=${PYTHON_LIB#lib}
+PYTHON_LIB_NAME=$(basename "${PYTHON_LIB}" | sed -E 's/^lib(.*)\.(so|dylib|a)$/\1/')
+ # Works for .so or .dylib
+
+# Compile C++ code
+g++ -O3 -g -fPIC -c hpbmodule.cpp -o hpbmodule.o -I${PYTHON_INCLUDE}
+
+# Export library path for linker
+export LIBRARY_PATH=$PREFIX/lib:$PYTHON_LIBDIR:$LIBRARY_PATH
+
+# Link shared library
+if [[ "$(uname)" == "Darwin" ]]; then
+  g++ -dynamiclib -o hpb.so hpbmodule.o reg_tet.o -L${PYTHON_LIBDIR} -l${PYTHON_LIB_NAME} -lgfortran
+else
+  g++ -shared -Wl,-soname,hpb.so -o hpb.so hpbmodule.o reg_tet.o -L${PYTHON_LIBDIR} -l${PYTHON_LIB_NAME} -lgfortran
+fi
+
+# Move built .so file to destination
 cp hpb.so ../
 
 cd -
+
+# Final installation
 $PYTHON setup.py build_ext --force
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
